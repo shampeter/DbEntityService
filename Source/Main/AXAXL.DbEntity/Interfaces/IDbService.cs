e50307d4c6e664c04330f79639b26b18a0fc0242
shampeter <sham.peter@gmail.com>
2019-08-22 11:30:10 -0400

    commenting

--- Source/Main/AXAXL.DbEntity/Interfaces/IDbService.cs
+++ Source/Main/AXAXL.DbEntity/Interfaces/IDbService.cs
@@ -8,6 +8,12 @@ namespace AXAXL.DbEntity.Interfaces
 {
 	public interface IDbService
 	{
+		/// <summary>
+		/// Boostrap DbService where the servie will scan <paramref name="assemblies"/> for class type which has the a <see cref="System.ComponentModel.DataAnnotations.Schema.TableAttribute"/> defined
+		/// and create <see cref="AXAXL.DbEntity.EntityGraph.Node"/> to store the discovered meta data.
+		/// </summary>
+		/// <param name="assemblies">list of assemblies to scan.  If none, full assemblies loaded will be used. <see cref="AppDomain.CurrentDomain"/></param>
+		/// <returns></returns>
 		IDbService Bootstrap(params Assembly[] assemblies);
 		IEnumerable<dynamic> FromRawSql(string rawQuery, IDictionary<string, object> parameters, string connectionName = null);
 		IQuery<T> Query<T>() where T : class, new();
