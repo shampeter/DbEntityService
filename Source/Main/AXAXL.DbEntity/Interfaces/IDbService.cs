0f3a6790855af5498fb444f78e44591bd1e3a072
shampeter <sham.peter@gmail.com>
2019-08-22 15:30:30 -0400

    completed interfac comments.

--- Source/Main/AXAXL.DbEntity/Interfaces/IDbService.cs
+++ Source/Main/AXAXL.DbEntity/Interfaces/IDbService.cs
@@ -15,8 +15,24 @@ namespace AXAXL.DbEntity.Interfaces
 		/// <param name="assemblies">list of assemblies to scan.  If none, full assemblies loaded will be used. <see cref="AppDomain.CurrentDomain"/></param>
 		/// <returns></returns>
 		IDbService Bootstrap(params Assembly[] assemblies);
+		/// <summary>
+		/// Run raw sql query.  See <see cref="IDatabaseDriver"/> for details.
+		/// </summary>
+		/// <param name="rawQuery"></param>
+		/// <param name="parameters"></param>
+		/// <param name="connectionName"></param>
+		/// <returns></returns>
 		IEnumerable<dynamic> FromRawSql(string rawQuery, IDictionary<string, object> parameters, string connectionName = null);
+		/// <summary>
+		/// Obtain an instance of IQuery to execute query on entity of type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">Entity class type</typeparam>
+		/// <returns>Return itself for chaining method calls.</returns>
 		IQuery<T> Query<T>() where T : class, new();
+		/// <summary>
+		/// Obtain an IPersist for saving entity objects as a unit.
+		/// </summary>
+		/// <returns>Return itself for chaining method calls.</returns>
 		IPersist Persist();
 	}
 }
--- Source/Main/AXAXL.DbEntity/Interfaces/IDbServiceOption.cs
+++ Source/Main/AXAXL.DbEntity/Interfaces/IDbServiceOption.cs
@@ -1,10 +1,34 @@
 ﻿namespace AXAXL.DbEntity.Interfaces
 {
+	/// <summary>
+	/// Server Option interface which define funcation to specify runtime database context such as connection string.
+	/// </summary>
 	public interface IDbServiceOption
 	{
+		/// <summary>
+		/// Assign or update a connection name with a connection string.
+		/// </summary>
+		/// <param name="connectionName">Name of connection</param>
+		/// <param name="connectionString">Database connection string</param>
+		/// <returns></returns>
 		IDbServiceOption AddOrUpdateConnection(string connectionName, string connectionString);
+		/// <summary>
+		/// Assign a connection name as a default.  Connection name is assigned to an entity object via <see cref="AXAXL.DbEntity.Annotation.ConnectionAttribute"/>.  If none is specified on an entity object,
+		/// this default will be used.
+		/// </summary>
+		/// <param name="connectionName"></param>
+		/// <returns></returns>
 		IDbServiceOption SetAsDefaultConnection(string connectionName);
+		/// <summary>
+		/// Return default connection string.
+		/// </summary>
+		/// <returns></returns>
 		string GetDefaultConnectionString();
+		/// <summary>
+		/// Return connection string by the specified connection name <paramref name="connectionName"/>.
+		/// </summary>
+		/// <param name="connectionName">Name for identifying a connection string as assigned in <see cref="AddOrUpdateConnection(string, string)"/></param>
+		/// <returns>Database connection string.</returns>
 		string GetConnectionString(string connectionName);
 	}
 }
