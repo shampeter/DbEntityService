836b99e28c858095e1e97136ade3fa31e6fcc74d
shampeter <sham.peter@gmail.com>
2019-10-20 03:49:11 -0400

    completed inner join map.

---
+++ Source/Main/AXAXL.DbEntity/MSSql/IInnerJoinMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AXAXL.DbEntity.EntityGraph;
+
+namespace AXAXL.DbEntity.MSSql
+{
+	internal interface IInnerJoinMap
+	{
+		/// <summary>
+		/// Initialize the map with root node of the query and its corresponding starting sequence number.
+		/// For example, if <paramref name="queryRootAliaxIdx"/> is 0, rest of table alias index will be 1, 2, 3 so on and so forth.
+		/// </summary>
+		/// <param name="queryRootNode">root node of a query.</param>
+		/// <param name="queryRootAliaxIdx">starting index of table alias sequence</param>
+		/// <returns>key to this map that can be used to retrieve the assigned table alias index.</returns>
+		string Init(Node queryRootNode, int queryRootAliaxIdx);
+		/// <summary>
+		/// Add the edge to the inner join map as specified by <paramref name="node"/> and <paramref name="entityRef"/>
+		/// </summary>
+		/// <param name="currentJoinKey">key to left hand side of the join table.</param>
+		/// <param name="node">node and <paramref name="entityRef"/> identify the edge for a join</param>
+		/// <param name="entityRef"><paramref name="node"/> and entityRef identify the edge for a join</param>
+		/// <returns>key to map that can be used to retrieve the assigned table alias index.</returns>
+		string Add(string currentJoinKey, Node node, string entityRef);
+		/// <summary>
+		/// Return the table alias assigned to an entity among the joins.
+		/// </summary>
+		/// <param name="mapKey">String.  Map key.</param>
+		/// <returns>Int.  Table alias index.</returns>
+		int GetAliasIndex(string mapKey);
+		IEnumerable<(int ParentTableAliasIdx, int ChildTableAliasIdx, NodeEdge Edge)> Joins { get;  }
+	}
+}
---
+++ Source/Main/AXAXL.DbEntity/MSSql/InnerJoinMap.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+using AXAXL.DbEntity.EntityGraph;
+
+namespace AXAXL.DbEntity.MSSql
+{
+	internal class InnerJoinMap : IInnerJoinMap
+	{
+		private int runningSequence;
+		private IDictionary<string, int> edgeKeyIndexes;
+		private IList<ValueTuple<int, int, NodeEdge>> innerJoins;
+
+		public InnerJoinMap()
+		{
+			this.edgeKeyIndexes = new Dictionary<string, int>();
+			this.innerJoins = new List<ValueTuple<int, int, NodeEdge>>();
+		}
+
+		public IEnumerable<(int ParentTableAliasIdx, int ChildTableAliasIdx, NodeEdge Edge)> Joins => this.innerJoins;
+
+		public string Add(string currentJoinKey, Node node, string newJoinEnittyRef)
+		{
+			var key = this.FormatEdgeKey(node, newJoinEnittyRef);
+			if (! this.edgeKeyIndexes.ContainsKey(key))
+			{
+				var currentAliasIdx = this.GetAliasIndex(currentJoinKey);
+				NodeEdge edge;
+				if (node.ContainsEdgeToChildren(newJoinEnittyRef))
+				{
+					edge = node.GetEdgeToChildren(newJoinEnittyRef);
+					this.innerJoins.Add((currentAliasIdx, this.runningSequence, edge));
+				}
+				else
+				{
+					edge = node.GetEdgeToParent(newJoinEnittyRef);
+					this.innerJoins.Add((this.runningSequence, currentAliasIdx, edge));
+				}
+				this.edgeKeyIndexes.Add(key, this.runningSequence++);
+			}
+			return key;
+		}
+
+		public int GetAliasIndex(string mapKey)
+		{
+			Debug.Assert(edgeKeyIndexes.ContainsKey(mapKey));
+			return this.edgeKeyIndexes[mapKey];
+		}
+
+		public string Init(Node queryRootNode, int queryRootAliaxIdx)
+		{
+			(int ParentTableAliasIdx, int ChildTableAliasIdx, NodeEdge Edge) topLevelJoin = (queryRootAliaxIdx, queryRootAliaxIdx, null);
+			var rootKey = this.FormatEdgeKey(queryRootNode, null, true);
+
+			this.runningSequence = queryRootAliaxIdx;
+			this.edgeKeyIndexes.Add(rootKey, this.runningSequence++);
+			this.innerJoins.Add(topLevelJoin);
+
+			return rootKey;
+		}
+
+		private string FormatEdgeKey(Node node, String entityRef, bool isRoot = false)
+		{
+			if (isRoot) return $"{node.Name}.-";
+
+			Debug.Assert(node.IsPropertyOnNode(entityRef) && (node.ContainsEdgeToChildren(entityRef) || node.ContainsEdgeToParent(entityRef)));
+			return $"{node.Name}.{entityRef}";
+		}
+	}
+}
